5335541058771589d468d155ce291259a289ce58
tomasherrera455 <tomasherrerao007@gmail.com>
2024-10-16 18:53:09 -0500

    g

---
+++ clase.js
@@ -0,0 +1,30 @@
+// tipos de variables 
+// var numero =4; = no se utiliza por actualizar 
+let saludo = "saludo";
+const pi = 3.1416;
+
+// tipos de datos
+//numeros
+
+ let entero = 10;
+ let decimal = 3.1416;
+
+//string = texto
+let pregunta = "¿Que hora es?"
+let respuesta = 'son las 4 pm'
+ 
+//boolean
+let esMayordeedad = true;
+let tienepermiso = false;
+
+//undefine
+let valor;
+console.long(valor); //undefine
+
+//null
+let nota = null;
+
+let persona= {
+    nombre: "juan",
+
+}
