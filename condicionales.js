d54df1c00aa76db4ad6fddf46b1d36c228c27a17
tomasherrera455 <tomasherrerao007@gmail.com>
2024-10-16 20:18:14 -0500

    a

---
+++ condicionales.js
@@ -0,0 +1,13 @@
+// estructuras condicionales
+
+// condicional if
+// if (condition) {
+// //codigo a ejecutar si la condicion es verdadera
+//}
+
+//la edad de una persona
+let edad= 18;
+
+if(edad >= 18){
+    console.log("eres mayor de edad");
+}
